91720f2d7a9bdbdecca52e6e8f61442cccb6fbe0
Abdul Rehman Shaikh <rehmanshaikh0796@gmail.com>
2022-06-28 11:07:48 +0530

    Changes done in api calling

--- lib/controller/other_utils_Controller/dropdown_controller/loading_lis_controller/loading_list_controller.dart
+++ lib/controller/other_utils_Controller/dropdown_controller/loading_lis_controller/loading_list_controller.dart
@@ -2,18 +2,25 @@ import 'dart:convert';
 
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
+import 'package:tranvision_customer_app/api_services/loading_list_api.dart';
+import 'package:tranvision_customer_app/controller/other_utils_Controller/dropdown_controller/loading_lis_controller/vessel_lis_controller.dart';
+import 'package:tranvision_customer_app/controller/other_utils_Controller/dropdown_controller/loading_lis_controller/voyage_lis_controller.dart';
 import 'package:tranvision_customer_app/model/loading_list_model/refresh_on_load_model.dart';
 import 'package:tranvision_customer_app/shared_preferences/shared_preference.dart';
 
 class LoadingListController extends GetxController {
+  UserLoginDetails userLoginDetails = UserLoginDetails();
   RxBool isVisible = false.obs;
   var loadingDataList = <MyLoadingList>[].obs;
+  VesselController vesselController = Get.put(VesselController());
+  VoyageController voyageController = Get.put(VoyageController());
 
   Future<List<MyLoadingList>> getLoadingDataApi() async {
-    UserLoginDetails userLoginDetails = UserLoginDetails();
     var username = userLoginDetails.retriveUserNameFromGetStrogare();
-    final response = await http.get(Uri.parse(
-        "http://192.168.1.143:9999/TSVAPI/sqlinterface.svc/LoadingList?vesselname=DSUNNY&voyagename=D048&partycode=P1210"));
+    final response = await http.get(Uri.parse(LoadingListApi.loadingListUrl(
+        username,
+        vesselController.vesselNewValue,
+        voyageController.selectedvalue)));
     var data = jsonDecode(response.body.toString());
     if (response.statusCode == 200) {
       loadingDataList.value = [];
