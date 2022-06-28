0568248dc66d477621883cab04ad432d57503414
Abdul Rehman Shaikh <rehmanshaikh0796@gmail.com>
2022-06-28 11:10:46 +0530

    dropdown changes down.

--- lib/utils/dropDown/voyage.dart
+++ lib/utils/dropDown/voyage.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:tranvision_customer_app/controller/other_utils_Controller/dropdown_controller/loading_lis_controller/voyage_lis_controller.dart';
 
 class VoyageDown extends StatelessWidget {
   final List<dynamic> listItems;
   final dynamic selectedValue;
+  final Function(dynamic value) notifyparent;
 
   const VoyageDown({
     Key? key,
     required this.listItems,
     this.selectedValue,
+    required this.notifyparent,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    VoyageController voyageController = Get.find();
     dynamic;
     return Container(
       padding: const EdgeInsets.all(10.0),
@@ -39,7 +38,7 @@ class VoyageDown extends StatelessWidget {
                 color: Colors.black, fontSize: 16, fontWeight: FontWeight.w600),
           ),
           onChanged: (value) {
-            voyageController.setValue(value);
+            notifyparent(value);
           },
         ),
       ),
